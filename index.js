35777608977b5e7b25c65b69d2a4d86fa2d42fa8
krishnas005 <kanavkrishna05@gmail.com>
2023-07-03 13:20:32 +0530

    new commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
     var randomDiceImage = "dice" + randomNumber1 + ".png";
-    var randomImageSource = "images/" + randomDiceImage;
+    var randomImageSource = randomDiceImage;
     var image1 = document.querySelectorAll("img")[0];
     image1.setAttribute("src", randomImageSource);
     var randomNumber2 = Math.floor(Math.random() * 6) + 1;
     var randomDiceImage = "dice" + randomNumber2 + ".png";
-    var randomImageSource = "images/" + randomDiceImage;
+    var randomImageSource = randomDiceImage;
     var image2 = document.querySelectorAll("img")[1];
     image2.setAttribute("src", randomImageSource);
     if(randomNumber1>randomNumber2){
